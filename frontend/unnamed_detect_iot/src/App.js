563105ce6126ad827361dbb2d15e11258d0d685d
herobrine369 <s1112233262@gmail.com>
2021-06-30 22:35:53 +0800

    Fix flickering text at frontend

--- frontend/unnamed_detect_iot/src/App.js
+++ frontend/unnamed_detect_iot/src/App.js
@@ -1,13 +1,19 @@
 import Dashboard from "./components/Dashboard";
+import {Redirect, Route, Switch} from "react-router";
+import { BrowserRouter as Router } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Dashboard/>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path={"/:pageNum"} children={<Dashboard/>}/>
+                <Route exact path={"/"}>
+                    <Redirect to={"/1"}/>
+                </Route>
+            </Switch>
+
+        </Router>
+    );
 }
 
 export default App;
--- frontend/unnamed_detect_iot/src/components/Dashboard.jsx
+++ frontend/unnamed_detect_iot/src/components/Dashboard.jsx
@@ -1,22 +1,33 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {useParams} from "react-router";
+import {Link} from "react-router-dom";
 
 const Dashboard = () => {
+    const {pageNum} = useParams()
     const [totalPageNum, setTotalPageNum] = useState(1)
-    const [pageNum, setPageNum] = useState(1)
-    const [personCounts, setPersonCounts] = useState([])
+    const [personCountsBody, setPersonCountsBody] = useState(<></>)
 
-    const update = () => {
+    // constantly update the table
+    useEffect(() => {
         axios.get(`persons-count/?page=${pageNum}`).then((res) => {
-            setPersonCounts(res.data.results)
+            let person_counts = res.data.results.map((personCount) => {
+                return (
+                    <tr className={"table-active"} key={personCount.time}>
+                        <th className="row">
+                            {personCount.time}
+                        </th>
+                        <td>
+                            {personCount.count}
+                        </td>
+                    </tr>
+                )
+            })
+
+            setPersonCountsBody(person_counts)
             setTotalPageNum(res.data.total_pages)
         })
-    }
-
-    useEffect(() => {
-        update()
-    })
+    }, [personCountsBody, totalPageNum])
 
     return (
         <div className={"container"}>
@@ -31,42 +42,30 @@ const Dashboard = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {personCounts.map((personCount) => {
-                    return (
-                        <tr className={"table-active"} key={personCount.time}>
-                            <th className="row">
-                                {personCount.time}
-                            </th>
-                            <td>
-                                {personCount.count}
-                            </td>
-                        </tr>
-                    )
-                })}
+                {personCountsBody}
                 </tbody>
             </table>
 
             {/*pagination*/}
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
-                    {pageNum > 1 && <li className="page-item"><a className="page-link" href="#"
-                                                                 onClick={() => setPageNum(pageNum - 1)}>Previous</a>
+                    {pageNum > 1 &&
+                    <li className="page-item"><Link className="page-link" to={'/' + (pageNum - 1)}>Previous</Link>
                     </li>}
                     {(() => {
                         let page_buttons = []
                         for (let i = 1; i <= totalPageNum; i++) {
-                            page_buttons.push(<li className={(i === pageNum ? "page-item" : "page-item active")}
-                                                  key={i}><a className="page-link"
-                                                             href="#"
-                                                             onClick={() => {
-                                                                 setPageNum(i)
-                                                             }}>{i}</a>
-                            </li>)
+                            page_buttons.push(
+                                <li className={(i === pageNum ? "page-item" : "page-item active")}
+                                    key={i}><Link className="page-link"
+                                                  to={'/' + i}>{i}</Link>
+                                </li>
+                            )
                         }
                         return page_buttons
                     })()}
-                    {pageNum < totalPageNum && <li className="page-item"><a className="page-link" href="#"
-                                                                            onClick={() => setPageNum(pageNum + 1)}>Next</a>
+                    {pageNum < totalPageNum && <li className="page-item"><Link className="page-link"
+                                                                               to={'/' + (pageNum + 1)}>Next</Link>
                     </li>}
                 </ul>
             </nav>

290a76d088fb216acddc09f0cc02237967743e9b
herobrine369 <s1112233262@gmail.com>
2021-07-01 20:44:31 +0800

    some todos added

--- frontend/unnamed_detect_iot/src/components/Dashboard.jsx
+++ frontend/unnamed_detect_iot/src/components/Dashboard.jsx
@@ -4,9 +4,13 @@ import {useParams} from "react-router";
 import {Link} from "react-router-dom";
 
 const Dashboard = () => {
-    const {pageNum} = useParams()
+    let {pageNum} = useParams()
     const [totalPageNum, setTotalPageNum] = useState(1)
     const [personCountsBody, setPersonCountsBody] = useState(<></>)
+
+    // FIXME REDIRECT NOT WORKING
+    // const [redirectHead, setRedirectHead] = useState(false)
+    // const [redirectTail, setRedirectTail] = useState({redirect:false, page:0})
 
     // constantly update the table
     useEffect(() => {
@@ -26,11 +30,34 @@ const Dashboard = () => {
 
             setPersonCountsBody(person_counts)
             setTotalPageNum(res.data.total_pages)
+
+            // FIXME REDIRECT NOT WORKING
+            // if (pageNum < 0) {
+            //     setRedirectHead(true)
+            // } else if (pageNum > totalPageNum) {
+            //     setRedirectTail({redirect:true, page:totalPageNum})
+            // }
+
+            // console.log(totalPageNum)
         })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [personCountsBody, totalPageNum])
 
     return (
         <div className={"container"}>
+
+            {/*If the page number is invalid, redirect to valid page*/}
+            {/*FIXME REDIRECT NOT WORKING*/}
+            {/*{(()=>{*/}
+            {/*    if (redirectHead){*/}
+            {/*        pageNum = 1*/}
+            {/*        return (<Redirect to={"/1"} />)*/}
+            {/*    } else if (redirectTail.redirect) {*/}
+            {/*        pageNum = redirectTail.page*/}
+            {/*        return (<Redirect to={"/" + pageNum} />)*/}
+            {/*    }*/}
+            {/*})()}*/}
+
             <h3>Persons count in some place</h3>
 
             {/*Persons count table*/}
@@ -50,22 +77,29 @@ const Dashboard = () => {
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
                     {pageNum > 1 &&
-                    <li className="page-item"><Link className="page-link" to={'/' + (pageNum - 1)}>Previous</Link>
+                    <li className="page-item">
+                        <Link className="page-link" to={'/' + (Number.parseInt(pageNum) - 1)}>
+                            Previous
+                        </Link>
                     </li>}
                     {(() => {
                         let page_buttons = []
                         for (let i = 1; i <= totalPageNum; i++) {
                             page_buttons.push(
-                                <li className={(i === pageNum ? "page-item" : "page-item active")}
-                                    key={i}><Link className="page-link"
-                                                  to={'/' + i}>{i}</Link>
+                                <li className={(i === pageNum ? "page-item" : "page-item active")} key={i}>
+                                    <Link className="page-link" to={'/' + i}>
+                                        {i}
+                                    </Link>
                                 </li>
                             )
                         }
                         return page_buttons
                     })()}
-                    {pageNum < totalPageNum && <li className="page-item"><Link className="page-link"
-                                                                               to={'/' + (pageNum + 1)}>Next</Link>
+                    {pageNum < totalPageNum &&
+                    <li className="page-item">
+                        <Link className="page-link" to={'/' + (Number.parseInt(pageNum) + 1)}>
+                            Next
+                        </Link>
                     </li>}
                 </ul>
             </nav>
